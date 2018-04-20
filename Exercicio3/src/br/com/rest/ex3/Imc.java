fdd3ff829295506ecfb115b29348a70ea7555f23
brulogatti <bru.logatti@gmail.com>
2018-04-19 21:48:08 -0300

    Segunda Versão

--- Exercicio3/src/br/com/rest/ex3/Imc.java
+++ Exercicio3/src/br/com/rest/ex3/Imc.java
@@ -10,16 +10,16 @@ public class Imc {
 	@GET
 	@Path("calcular/{param1}/{param2}/{param3}/{param4}")
 	public void calcular(@PathParam ("param1") String nome,  @PathParam ("param2") int idade, @PathParam ("param3") double peso, @PathParam ("param4") double altura) {
-		double imcd = peso/(altura*altura);
+		double imc = peso/(altura*altura);
 		
 		
 	}
 	
-	@GET
+	/*@GET
 	@Path("mostrar")
 	public String mostrar() {
 		
 		
-	}
+	}*/
 
 }
