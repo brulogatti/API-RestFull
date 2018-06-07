e6802815083b15fee46614111a928f667fb9b160
brulogatti <bru.logatti@gmail.com>
2018-06-07 20:13:49 -0300

    Aula 07/06

--- ReservarSala/src/IncluirReserva.java
+++ ReservarSala/src/IncluirReserva.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -8,33 +9,43 @@ import java.net.URL;
 public class IncluirReserva {
 	
 public static void main(String[]args) {
-		
-		try {
-			
-			URL url = new URL ("http://localhost:8080/ProjReservarSala/rest/json/incluir");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-			
-			if(conn.getResponseCode()!=200) {
-				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-			}
-			
-			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-			
-			String output;
-			System.out.println("Output from Server .... \n");
-			while((output=br.readLine())!=null) {
-				System.out.println(output);
-			}
-			
-			conn.disconnect();
-		}catch (MalformedURLException e) {
-			e.printStackTrace();
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
 	
-
+	try {
+		
+		URL url = new URL(
+				"http://localhost:8080/ProjReservarSala/rest/json/get");
+		
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		
+		String input = "{\"id\":2,\"descricao\":\"Sala 2, 200 lugares\",\"datacadastro\":\"05/05\",\"datareserva\":\"10/05\",\"hora\":\"19:30\"}";
+		
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+		
+		if(conn.getResponseCode()!=HttpURLConnection.HTTP_CREATED) {
+			throw new RuntimeException("Failed : HTTP error code : "+conn.getResponseCode());
+		}
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+		
+		String output;
+		System.out.println("Output from Server .... \n");
+		while((output = br.readLine())!=null) {
+			System.out.println(output);
+		}
+		
+		conn.disconnect();
+		
+		
+	}catch (MalformedURLException e) {
+		e.printStackTrace();
+	}catch (IOException e) {
+		e.printStackTrace();
+	}
 }
+		
 }
